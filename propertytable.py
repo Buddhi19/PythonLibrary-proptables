6da5073ee0dcb2674d4f366c057a4f8d1b814410
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-06-04 00:18:59 +0530

    updated

--- propertytable.py
+++ propertytable.py
@@ -1,104 +1,33 @@
 import pandas as pd
 from calSuperHeatData import HeatedCal
+from calSatData import SatData
 
-class R134a:
-    def __init__(self):
-        self.dfPressure=pd.read_csv("./R134a_PresSat.csv")
-        self.dfPressure=self.dfPressure.iloc[: , :-1]
-        self.dfTemperature=pd.read_csv("./R134a_TempSat.csv")
-        self.dfTemperature=self.dfTemperature.iloc[: , :-1]
-
-
-    def FindbyTemp(self,Temperature):
-        if Temperature in self.dfTemperature["degC"].unique():
-            indexing=self.dfTemperature[self.dfTemperature["degC"]==Temperature].index.values
-            row=self.dfTemperature.iloc[indexing]
-            return row
-        else:
-            nearest=self.dfTemperature["degC"].sub(Temperature).abs().argsort()[:2]
-            result=self.dfTemperature.iloc[nearest]
-            result=result.copy()
-            result.loc[-1,"degC"]=Temperature
-            result =result.sort_values(by='degC')
-            result=result.reset_index(drop=True)
-            result.set_index('degC', inplace=True)
-            result.interpolate(method='index', inplace=True)
-            result.reset_index(inplace=True)
-            return result
-            
-
-
-    def FindbyPressure(self,Pressure):
-        if Pressure in self.dfPressure["kPa"].unique():
-            indexing=self.dfPressure[self.dfPressure["kPa"]==Pressure].index.values
-            row=self.dfPressure.iloc[indexing]
-            return row
-        else:
-            nearest=self.dfPressure["kPa"].sub(Pressure).abs().argsort()[:2]
-            result=self.dfPressure.iloc[nearest]
-            result=result.copy()
-            result.loc[-1,"kPa"]=Pressure
-            result =result.sort_values(by='kPa')
-            result=result.reset_index(drop=True)
-            result.set_index('kPa', inplace=True)
-            result.interpolate(method='index', inplace=True)
-            result.reset_index(inplace=True)
-            return result
-
-
-
-    def calculate_x_temp(self,Temperature,Enthalpy):
-        result=self.FindbyTemp(Temperature)
-        result=pd.DataFrame(result)
-        if result.shape[0]==3:        
-            result.drop([0],inplace=True)
-            result=result.reset_index(drop=True)
-            result.drop([1],inplace=True)
-            result=result.reset_index(drop=True)
-        Hf=result.iat[0,6]
-        Hg=result.iat[0,8]
-        x=(Enthalpy-Hf)/Hg
-        result["x"]=x
-        return result
+def R134a(Temperature=None,Pressure=None,Enthalpy=None,Entropy=None,specificvolume=None,Superheated=None):
     
-    def calculate_x_pressure(self,Pressure,Enthalpy):
-        result=self.FindbyPressure(Pressure)
-        if result.shape[0]==3:        
-            result.drop([0],inplace=True)
-            result=result.reset_index(drop=True)
-            result.drop([1],inplace=True)
-            result=result.reset_index(drop=True)
-        Hf=result.iat[0,6]
-        Hg=result.iat[0,8]
-        x=(Enthalpy-Hf)/Hg
-        result["x"]=x
-        return result
-
-    def values(self,Temperature=None,Pressure=None,Enthalpy=None,Entropy=None,specificvolume=None,Superheated=None):
-        if Temperature and Pressure:
-            return HeatedCal.findsuperTemp(Pressure,Temperature)
+    if Temperature and Pressure:
+        return HeatedCal.findsuperTemp(Pressure,Temperature)
+    
+    if Superheated and Pressure and Enthalpy:
+        return HeatedCal.findsuperEnthalpy(Pressure,Enthalpy)
+    
+    if Superheated and Pressure and Entropy:
+        return HeatedCal.findsuperEntropy(Pressure,Entropy)
+    
+    if Superheated and Pressure and specificvolume:
+        return HeatedCal.findsuperspecificvolume(Pressure,specificvolume)
+    
+    if Superheated and Pressure:
+        return HeatedCal.superheatedTable(Pressure)
+    
+    if Temperature and Enthalpy:
+        return(SatData.calculate_x_temp(Temperature,Enthalpy))
         
-        if Superheated and Pressure and Enthalpy:
-            return HeatedCal.findsuperEnthalpy(Pressure,Enthalpy)
+    if Pressure and Enthalpy:
+        return(SatData.calculate_x_pressure(Pressure,Enthalpy))
         
-        if Superheated and Pressure and Entropy:
-            return HeatedCal.findsuperEntropy(Pressure,Entropy)
+    if Temperature:
+        return(SatData.FindbyTemp(Temperature))
         
-        if Superheated and Pressure and specificvolume:
-            return HeatedCal.findsuperspecificvolume(Pressure,specificvolume)
-        
-        if Superheated and Pressure:
-            return HeatedCal.superheatedTable(Pressure)
-        
-        if Temperature and Enthalpy:
-            return(self.calculate_x_temp(Temperature,Enthalpy))
-            
-        if Pressure and Enthalpy:
-            return(self.calculate_x_pressure(Pressure,Enthalpy))
-            
-        if Temperature:
-            return(self.FindbyTemp(Temperature))
-            
-        if Pressure:
-            return(self.FindbyPressure(Pressure))
+    if Pressure:
+        return(SatData.FindbyPressure(Pressure))
 
--- test.py
+++ test.py
@@ -1,34 +1,33 @@
 from propertytable import R134a
 
-r=R134a()
 
 #################### Check for Temp Saturated Table####################
 
 check_1=False
 
 if check_1:
-    print(r.values(Temperature=50))
+    print(R134a(Temperature=50))
 
 ###################### Check for Pressure Saturated Table  ###############
 
 check_2=False
 
 if check_2:
-    print(r.values(Pressure=230))
+    print(R134a(Pressure=230))
 
 #######################3 Check for Saturated liquid Temp ##################
 
 check_3=False
 
 if check_3:
-    print(r.values(Temperature=40,Enthalpy=300))
+    print(R134a(Temperature=40,Enthalpy=300))
 
 ####################### check for Saturated liquid Pressure ##############
 
 check_4=False
 
 if check_4:
-    print(r.values(Pressure=1334,Enthalpy=223))
+    print(R134a(Pressure=1334,Enthalpy=223))
 
 ##################### check for Superheated Vapour #####################
 
@@ -37,13 +36,13 @@ check_5=False
 pre=[60,100,140,180,200,240,280,320,400,500,600,700,800,900,1000,1200,1400,1600,1800,2000]
 
 if check_5:
-    print(r.values(Pressure=60,Superheated=True))
+    print(R134a(Pressure=60,Superheated=True))
 
 check_5_sub=False
 
 if check_5_sub:
     for num in pre:
-        print(r.values(Pressure=num,Superheated=True))
+        print(R134a(Pressure=num,Superheated=True))
 
 
 ########################## Check for superheated pressure and Temperature#######
@@ -51,25 +50,25 @@ if check_5_sub:
 check_6=False
 
 if check_6:
-    print(r.values(Pressure=100,Temperature=95))
+    print(R134a(Pressure=100,Temperature=95))
 
 ########################### Check for superheated pressure and enthalpy ##########
 
-check_7=False
+check_7=True
 
 if check_7:
-    print(r.values(Pressure=240,Enthalpy=331,Superheated=True))
+    print(R134a(Pressure=240,Enthalpy=331,Superheated=True))
 
 ########################### Check for superheated pressure and entropy ##########
 
 check_8=False
 
 if check_8:
-    print(r.values(Pressure=400,Entropy=1.2,Superheated=True))
+    print(R134a(Pressure=400,Entropy=1.2,Superheated=True))
 
 ########################### Check for superheated pressure and specificvolume ##########
 
 check_9=False
 
 if check_9:
-    print(r.values(Superheated=True,Pressure=400,specificvolume=0.06))
+    print(R134a(Superheated=True,Pressure=400,specificvolume=0.06))
