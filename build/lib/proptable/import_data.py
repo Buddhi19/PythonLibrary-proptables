e980ac7d1e0af345f7615d0b12cc2eec411d03f4
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-06-05 16:56:02 +0530

    fix

---
+++ build/lib/proptable/import_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from pathlib import Path
+
+data_path = Path(Path.cwd(), 'proptable', 'R134a_Super.csv')
+
+print(data_path)
---
+++ build/lib/proptable/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from proptable.calSuperHeatData import HeatedCal
+from proptable.calSatData import SatData
+
+
+def R134a(Temperature=None,Pressure=None,Enthalpy=None,Entropy=None,specificvolume=None,Superheated=None):
+    
+    if Temperature and Pressure:
+        return HeatedCal.findsuperTemp(Pressure,Temperature)
+    
+    if Superheated and Pressure and Enthalpy:
+        return HeatedCal.findsuperEnthalpy(Pressure,Enthalpy)
+    
+    if Superheated and Pressure and Entropy:
+        return HeatedCal.findsuperEntropy(Pressure,Entropy)
+    
+    if Superheated and Pressure and specificvolume:
+        return HeatedCal.findsuperspecificvolume(Pressure,specificvolume)
+    
+    if Superheated and Pressure:
+        return HeatedCal.superheatedTable(Pressure)
+    
+    if Temperature and Enthalpy:
+        return(SatData.calculate_x_temp(Temperature,Enthalpy))
+        
+    if Pressure and Enthalpy:
+        return(SatData.calculate_x_pressure(Pressure,Enthalpy))
+        
+    if Temperature:
+        return(SatData.FindbyTemp(Temperature))
+        
+    if Pressure:
+        return(SatData.FindbyPressure(Pressure))
+
